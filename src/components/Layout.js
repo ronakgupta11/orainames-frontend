5693a9aa095ad502dbf005a5325d1c9b6c7a1712
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2024-08-15 19:32:04 +0530

    add cosmos kit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,21 @@ import Head from 'next/head';
 import Link from 'next/link';
 import NavBarComp from './NavBarComp';
 import Footer from './Footer';
+import { ChainProvider } from '@cosmos-kit/react';
+import { chains, assets } from 'chain-registry';
+import { wallets } from '@cosmos-kit/keplr';
+ 
+// Import this in your top-level route/layout
+import "@interchain-ui/react/styles";
+
 const Layout = ({ children, title = 'Orai Names' }) => {
   return (
+<ChainProvider
+      chains={chains} // supported chains
+      assetLists={assets} // supported asset lists
+      wallets={wallets} // supported wallets
+    // required if `wallets` contains mobile wallets
+    >
     <div >
       <Head>
         <title>{title}</title>
@@ -17,6 +30,7 @@ const Layout = ({ children, title = 'Orai Names' }) => {
       <main className='min-h-screen'>{children}</main>
 <Footer/>
     </div>
+    </ChainProvider>
   );
 };
 
